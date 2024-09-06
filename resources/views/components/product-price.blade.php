c70e8ddb2a8b7ca0c234fa495e928f1d5eb61a5a
Brian Thomas <56766194+brianbct129@users.noreply.github.com>
2024-09-06 17:44:25 +0800

    config: lunar laravel

--- resources/views/components/product-price.blade.php
+++
@@ -1,3 +0,0 @@
-<span {{ $attributes }}>
-    {{ $price?->price->formatted() }}
-</span>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,5 @@
 <?php
 
-use App\Livewire\CheckoutPage;
-use App\Livewire\CheckoutSuccessPage;
-use App\Livewire\CollectionPage;
-use App\Livewire\Home;
-use App\Livewire\ProductPage;
-use App\Livewire\SearchPage;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,19 +8,11 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
-Route::get('/', Home::class);
-
-Route::get('/collections/{slug}', CollectionPage::class)->name('collection.view');
-
-Route::get('/products/{slug}', ProductPage::class)->name('product.view');
-
-Route::get('search', SearchPage::class)->name('search.view');
-
-Route::get('checkout', CheckoutPage::class)->name('checkout.view');
-
-Route::get('checkout/success', CheckoutSuccessPage::class)->name('checkout-success.view');
+Route::get('/', function () {
+    return view('welcome');
+});
